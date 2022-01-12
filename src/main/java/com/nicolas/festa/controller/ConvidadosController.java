4681fa767c0e085b64a0566c1d29dd77e836df11
Nicolas Portela <nmpc1997@gmail.com>
2022-01-12 11:56:55 -0300

    projeto spring base, primeiro commit

---
+++ src/main/java/com/nicolas/festa/controller/ConvidadosController.java
@@ -0,0 +1,13 @@
+package com.nicolas.festa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ConvidadosController {
+
+    @GetMapping("/convidados")
+    public String listar(){
+        return "ListaConvidados";
+    }
+}
---
+++ src/test/java/com/nicolas/festa/GestaoFestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas.festa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
